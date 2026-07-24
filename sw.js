c54ef85b08e448940fcb7acb05db0ab0e310548a
becomerichmond-maker <becomerichmond@gmail.com>
2026-07-24 14:47:44 +0800

    Update cache version and add subpage to app shell

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'ai-supermarket-v1';
-const APP_SHELL = ['./', './index.html', './ai-supermarket-homepage.html', './manifest.webmanifest'];
+const CACHE_NAME = 'ai-supermarket-v2';
+const APP_SHELL = ['./', './index.html', './ai-supermarket-homepage.html', './subpage.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
