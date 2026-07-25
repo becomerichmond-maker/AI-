ceee75a64ce9d3c3daa3735d60cdb88021a7699a
becomerichmond-maker <becomerichmond@gmail.com>
2026-07-25 17:57:03 +0800

    Update service worker to version 3 and add activation

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
-const CACHE_NAME = 'ai-supermarket-v2';
-const APP_SHELL = ['./', './index.html', './ai-supermarket-homepage.html', './subpage.html', './manifest.webmanifest'];
+const CACHE_NAME = "ai-supermarket-v3";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./ai-supermarket-homepage.html",
+  "./subpage.html",
+  "./manifest.webmanifest"
+];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
 });
